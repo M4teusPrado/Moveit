c1f0eb280db0530e8fda6e54a1b3dc206d922cf3
Mateus Prado <mateus.silvaprado@gmail.com>
2021-02-25 21:14:11 -0300

    add ChallengeBox

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,6 +1,8 @@
 import style from '../styles/components/CompletedChallenges.module.css'
 
 
+
+
 export function CompletedChallenges(){
     return(
         <div className={style.CompletedChallengesContainer}>
